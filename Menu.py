768af3e36295b9187502f38a03582163d1b17a80
Alex David Frías <adavidfruiz@gmail.com>
2025-01-28 12:47:49 -0500

    feat: se implemento interfaz gráfica para mostrar productos en el menú

--- Menu.py
+++ Menu.py
@@ -1,4 +1,34 @@
 from inventario import productos
-from imprimir import imprimirProductos
+import tkinter as tk
+from tkinter import ttk
 
-imprimirProductos(productos)
+def mostrarInterfaz():
+
+    ventana = tk.Tk()
+    ventana.title("MercaSys")
+
+    columnas = ("ID", "Nombre", "Cantidad", "Unidad", "Precio (USD)", "Categoría")
+    tabla = ttk.Treeview(ventana, columns=columnas, show="headings")
+
+    for col in columnas:
+        tabla.heading(col, text=col)
+        tabla.column(col, anchor=tk.CENTER, width=120)
+
+    for idProducto, detalles in productos.items():
+        tabla.insert("", tk.END, values=(
+            idProducto,
+            detalles["Nombre"],
+            detalles["Cantidad"],
+            detalles["Unidad"],
+            f"${detalles['Precio']:.2f}",
+            detalles["Categoría"]
+        ))
+
+    tabla.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+    ventana.mainloop()
+
+mostrarInterfaz()
+
+
+
